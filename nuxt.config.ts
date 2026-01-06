39b578b73d6e14fe6938005357233c4130049af7
Usman Bala <Usman Bala>
2026-01-06 11:57:48 +0100

    prepared for paes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  nitro: {
+    output: {
+      dir: 'docs',
+      publicDir: 'docs'
+    }
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/hints',
